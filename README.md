ca9235697ca3d42f2254f13a9f5a50536b402e7c
Hiroshi Shimamoto <hshimamoto@users.noreply.github.com>
2020-08-04 14:00:02 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+binary files
+============
+
+- go-toybox
+  * bashhistcompact
+  * fwd
+  * fwdset
+  * golangbuilder
+  * gradlebuilder
+  * sshfwd
+
+- univproxy
+  * univproxy
+
+- others
+  * sshproxy
+
+License
+-------
+
+### Binary files
+See each binary file
+
+### Install script
+MIT License Copyright(c) 2020 Hiroshi Shimamoto
---
+++ inst.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+cd $(dirname $0)
+
+if [ ! -e binaries.tar.xz ]; then
+	echo "no binaries.tar.xz"
+	exit 1
+fi
+
+tar Jxf binaries.tar.xz
+# extracted to bin/ directory
+
+if [ ! -d bin ]; then
+	echo "Something wrong"
+	exit 1
+fi
+
+cd bin
+
+dest=$HOME/bin
+
+if [ ! -d $dest ]; then
+	mkdir $dest
+fi
+
+# check install command
+which install > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+	instcmd="install -p"
+else
+	instcmd="cp -p"
+fi
+
+find -type f | sort | while read f; do
+	echo "install $(basename $f)"
+	$instcmd $f $dest/
+done
